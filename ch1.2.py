94ce265bc2a97127110d808cc842bb7d81693128
Tim Hoolihan <tim@hoolihan.net>
2016-11-02 19:56:15 -0400

    Show text messages, next to analyze if there is a diff PMF

---
+++ ch1.2.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+plt.style.use('ggplot')
+
+colors = ["#348ABD", "#A60628"]
+
+count_data = np.loadtxt("data/txtdata.csv")
+n_count_data = len(count_data)
+
+plt.bar(np.arange(n_count_data), count_data, color = colors[0])
+
+
+plt.xlabel("Time (days)")
+plt.ylabel("Text messages received")
+plt.title("Did user's habits change")
+plt.show()
