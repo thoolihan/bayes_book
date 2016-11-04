14ff1db8dc6dc5903b3943234a4c4087284209b1
Tim Hoolihan <tim.hoolihan@dialogtech.com>
2016-11-04 16:00:23 -0400

    Text example working

--- ch1.3.py
+++ ch1.3.py
@@ -1,5 +1,9 @@
 import numpy as np
 import pymc as pm
+from IPython.core.pylabtools import figsize
+import matplotlib.pyplot as plt
+
+colors = ["#348ABD", "#A60628", "#7A68A6", "#467821"]
 
 count_data = np.loadtxt("BookSource/Chapter1_Introduction/data/txtdata.csv")
 n_count_data = len(count_data)
@@ -21,3 +25,46 @@ def lambda_(tau = tau, lambda_1 = lambda_1, lambda_2 = lambda_2):
 observation = pm.Poisson("obs", lambda_, value= count_data, observed = True)
 
 model = pm.Model([observation, lambda_1, lambda_2, tau])
+mcmc = pm.MCMC(model)
+mcmc.sample(40000, 10000)
+
+lambda_1_samples = mcmc.trace('lambda_1')[:]
+lambda_2_samples = mcmc.trace('lambda_2')[:]
+tau_samples = mcmc.trace('tau')[:]
+
+figsize(14.5, 10)
+
+ax = plt.subplot(311)
+ax.set_autoscaley_on(False)
+
+plt.hist(lambda_1_samples, histtype='stepfilled', bins=31, alpha=0.85,
+         label="posterior of $\lambda1$", color=colors[1], normed=True)
+plt.legend(loc="upper left")
+plt.title(r"""Posterior distributions of the parameters\
+$\lambda_1,\;\lambda_2,\;\tau$""")
+plt.xlim([15, 30])
+plt.xlabel("$\lambda_1$ value")
+plt.ylabel("Density")
+
+ax = plt.subplot(312)
+ax.set_autoscaley_on(False)
+plt.hist(lambda_2_samples, histtype='stepfilled', bins=31, alpha=0.85,
+         label="posterior of $\lambda2$", color=colors[2], normed=True)
+plt.legend(loc="upper left")
+plt.xlim([15, 30])
+plt.xlabel("$\lambda_2$ value")
+plt.ylabel("Density")
+
+plt.subplot(313)
+w = 1.0 / tau_samples.shape[0] * np.ones_like(tau_samples)
+plt.hist(tau_samples, bins=n_count_data, alpha=1,
+         label="posterior of $\tau$", color=colors[3],
+         weights=w, rwidth=2)
+plt.xticks(np.arange(n_count_data))
+plt.legend(loc="upper left")
+plt.ylim([0, .75])
+plt.xlim([35, len(count_data)-20])
+plt.xlabel(r"$\tau$ (in days)")
+plt.ylabel("Probability")
+
+plt.show()
