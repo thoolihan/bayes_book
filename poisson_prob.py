1e524dabe7585ac7051c23d05d6df7e121c3e185
Tim Hoolihan <tim.hoolihan@dialogtech.com>
2016-11-22 18:09:55 -0500

    Looking at poisson distribution

---
+++ poisson_prob.py
@@ -0,0 +1,19 @@
+import numpy as np
+from scipy.special import factorial
+
+def poisson_probability(actual, mean):
+    #overflows: (mean ** candidate * np.e ** (-1 * mean)) / factorial(candidate)
+    p = np.e ** -mean
+    for i in range(actual):
+        p *= mean
+        p /= i+1
+    return p
+
+def poisson_map(a, mean):
+    return [poisson_probability(mean, c) for c in a]
+
+if __name__ == "__main__":
+    mean = 2
+    candidate = 4
+    print("Weekly average: {average}, Probability of {candidate} is {probability:.2f}%".format(
+        average = mean, candidate = candidate, probability = poisson_probability(candidate, mean) * 100))
